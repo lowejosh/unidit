cb09fb654853f4c21889acb9348340ab2f7ab1ed
josh <lowejosh50@gmail.com>
2019-05-14 20:35:00 +1100

    added create thread form

--- src/containers/SideBarTop.js
+++ src/containers/SideBarTop.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Form, Button} from 'react-bootstrap';
+import CreateThread from './CreateThread';
 
 const SideBarTop = (props) => {
+    const [modalShow, setModalShow] = useState(false);
+    const modalClose = () => {setModalShow(false)};
+
     return (
         <div>
             <Form className="my-2">
@@ -9,9 +13,12 @@ const SideBarTop = (props) => {
                     <Form.Control type="text" placeholder="Search for threads..." />
                 </Form.Group>
             </Form>
-            <Button variant="custom-primary w-100 mb-2" className="custom-primary" type="submit">
+            <Button variant="custom-primary w-100 mb-2" className="custom-primary" onClick={() => {
+                setModalShow(true);
+            }}>
                 Create Thread
             </Button>
+            <CreateThread show={modalShow} onHide={modalClose} />
         </div>
     );
 }
